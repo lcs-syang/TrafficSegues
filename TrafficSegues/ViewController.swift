1bb0c4b0ed475f6a5d6b617eda6d729265663051
Selena Yang <syang@lcs.on.ca>
2019-11-13 19:18:04 -0500

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Yang, Selena on 2019/11/13.
+//  Copyright © 2019 Yang Selena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
